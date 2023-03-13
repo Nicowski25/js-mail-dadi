2e3ca4e1181333dfba03ff95d24ce58af4774681
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-13 15:17:20 +0100

    email checker on html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,29 @@
 //creiamo lista email che hanno il permesso
 const emailAllowed = [
     'nicola.faedo@gmail.com',
-    'nicola@boolean.com'
+    'nicola@boolean.com',
+    'tizio@caio.com'
 ]
-//facciamo inserire all'utente la mail
-const emailUser = prompt('inserisci la tua email')
-//controlliamo se l'email è nella lista di quelle che hanno il permesso
-for (let i = 0; i < emailAllowed.length; i++) {
-    if (emailAllowed[i] == emailUser) {
-        alert('l\'email ha il permesso')
-    } else {
-        alert('l\'email non ha il permesso')
+console.log(emailAllowed);
+//creiamo la variabile che punta al button emailcheck
+const emailCheck = document.getElementById("emailCheck")
+//al click sul bottone controlliamo se è l'email è in lista
+emailCheck.addEventListener('click',
+    function () {
+        //prendiamo la mail inserita nell'input
+        const emailUser = document.getElementById("email").value
+        console.log(emailUser);
+        for (let i = 0; i < emailAllowed.length; i++) {
+                if (emailAllowed[i] == emailUser) {
+                    alert('l\'email ha il permesso')
+                    console.log('permesso');
+                    break
+                } else {
+                    console.log('non ha il permesso');
+                }
+        }
     }
-}
+)
+//troviamo il bottone play nella dom
+const playButton = document.getElementById("play")
+//alla click sul bottone iniziamo la nostra funzione
